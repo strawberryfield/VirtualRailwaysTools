1c05f1b0bc437daf4ac9d300db6ef2866d2b6fc9
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2017-04-29 09:16:24 +0200

    Automatic site update

---
+++ doc_src/makesite.bat
@@ -0,0 +1,18 @@
+@echo off
+
+REM script for building website to host on github
+
+set builddir=_build
+set site=..\docs
+rem set downloads=%site%\bin
+
+call make.bat clean
+call make.bat html
+xcopy %builddir%\html %site%  /S /Y 
+
+rem call make.bat latexpdf
+rem copy /Y %builddir%\latex\*.pdf %downloads%
+
+git add .
+git commit -m "Automatic site update"
+git push origin master
