3df908eeac3ea2c1a980ec2fa88f582b4b2c7ff0
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2017-06-01 21:27:10 +0200

    Added ORTS parser for MSTS

---
+++ ORTS Libs/Orts.Parsers.Msts/XNAworkaround.cs
@@ -0,0 +1,44 @@
+﻿// copyright (c) 2017 Roberto Ceccarelli - CasaSoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of CasaSoft Virtual Railways Tools
+// 
+// CasaSoft Virtual Railways Tools is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CasaSoft Virtual Railways Tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CasaSoft Virtual Railways Tools.  
+// If not, see <http://www.gnu.org/licenses/>.
+
+namespace Orts.Parsers.Msts
+{
+    public class Vector3
+    {
+        public float X;
+        public float Y;
+        public float Z;
+    }
+
+    public class Vector2
+    {
+        public float X;
+        public float Y;
+    }
+
+    public class Vector4
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public float W;
+    }
+
+}
