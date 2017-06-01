fc1e780d85569bb41d7a721a5c976b808cc37e8c
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2017-06-01 22:03:08 +0200

    Shape Tools template

---
+++ Modeling Tools/st/stForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿// copyright (c) 2017 Roberto Ceccarelli - CasaSoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of CasaSoft Virtual Railways Tools
+// 
+// CasaSoft Virtual Railways Tools is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CasaSoft Virtual Railways Tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CasaSoft Virtual Railways Tools.  
+// If not, see <http://www.gnu.org/licenses/>.
+
+namespace CasaSoft.vrt.Modeling
+{
+    partial class stForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "stForm";
+        }
+
+        #endregion
+    }
+}
