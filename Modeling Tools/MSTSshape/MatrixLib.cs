230fbf191ade6e115edaffab9cc0144777ce3a30
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2017-06-14 22:26:19 +0200

    Completed points computing

--- Modeling Tools/MSTSshape/MatrixLib.cs
+++ Modeling Tools/MSTSshape/MatrixLib.cs
@@ -18,29 +18,57 @@
 // along with CasaSoft Virtual Railways Tools.  
 // If not, see <http://www.gnu.org/licenses/>.
 
-using Orts.Parsers.Msts;
 using Orts.Formats.Msts;
 
 namespace CasaSoft.vrt.Modeling
 {
+    /// <summary>
+    /// Extended <see cref="T:Orts.Formats.Msts.point"/> class
+    /// </summary>
     public class PointEx
     {
+        /// <summary>
+        /// <see cref="T:Orts.Formats.Msts.point"/> instance
+        /// </summary>
         protected point Point;
 
+        /// <summary>
+        /// X coord
+        /// </summary>
         public float X { get { return Point.X; } set { Point.X = X; } }
+
+        /// <summary>
+        /// Y coord
+        /// </summary>
         public float Y { get { return Point.Y; } set { Point.Y = Y; } }
+
+        /// <summary>
+        /// Z coord
+        /// </summary>
         public float Z { get { return Point.Z; } set { Point.Z = Z; } }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public PointEx()
         {
             Point = new point();
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="p"></param>
         public PointEx(point p)
         {
             Point = p;
         }
 
+        /// <summary>
+        /// Sum a vector
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
         public point Sum(point p)
         {
             point ret = new point(p.X, p.Y, p.Z);
@@ -52,20 +80,38 @@ namespace CasaSoft.vrt.Modeling
     }
 
 
+    /// <summary>
+    /// Extended <see cref="T:Orts.Formats.Msts.matrix"/> class
+    /// </summary>
     public class MatrixEx
     {
+        /// <summary>
+        /// <see cref="T:Orts.Formats.Msts.matrix"/> instance
+        /// </summary>
         protected matrix mat;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public MatrixEx()
         {
             mat = new matrix();
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="m"></param>
         public MatrixEx(matrix m)
         {
             mat = m;
         }
 
+        /// <summary>
+        /// Tests if the given <see cref="T:Orts.Formats.Msts.matrix"/> is unitary
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
         public bool isUnity(matrix m)
         {
             return (m.AX == 1 & m.AY == 0 & m.AZ == 0
@@ -73,11 +119,21 @@ namespace CasaSoft.vrt.Modeling
                 & m.CX == 0 & m.CY == 0 & m.CZ == 1);
         }
 
+        /// <summary>
+        /// Text if this matrix is unitary
+        /// </summary>
+        /// <returns></returns>
         public bool isUnity()
         {
             return isUnity(mat);
         }
 
+        /// <summary>
+        /// multyply this matrix by the given <see cref="T:Orts.Formats.Msts.matrix"/>
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        /// <remarks>Also sums the offset vector</remarks>
         public matrix Mul(matrix m)
         {
             matrix ret;
@@ -104,9 +160,20 @@ namespace CasaSoft.vrt.Modeling
                     }
                 }
             }
+            PointEx pe = new PointEx(new point(m.DX, m.DY, m.DZ));
+            point p = pe.Sum(new point(mat.DX, mat.DY, mat.DZ));
+            ret.DX = p.X;
+            ret.DY = p.Y;
+            ret.DZ = p.Z;
             return ret;
         }
 
+        /// <summary>
+        /// Multiply the given <see cref="T:Orts.Formats.Msts.point"/> by this matrix
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        /// <remarks>Also sums the offset vector</remarks>
         public point Mul(point p)
         {
             point ret;
@@ -120,7 +187,8 @@ namespace CasaSoft.vrt.Modeling
                     p.X * mat.AY + p.Y * mat.BY + p.Z * mat.CY,
                     p.X * mat.AZ + p.Y * mat.BZ + p.Z * mat.CZ);
             }
-            return ret;
+            PointEx pe = new PointEx(ret);
+            return pe.Sum(new point(mat.DX, mat.DY, mat.DZ));
         }
     }
 }
--- Modeling Tools/MSTSshape/MstsShape.cs
+++ Modeling Tools/MSTSshape/MstsShape.cs
@@ -18,10 +18,7 @@
 // along with CasaSoft Virtual Railways Tools.  
 // If not, see <http://www.gnu.org/licenses/>.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Orts.Parsers.Msts;
 using Orts.Formats.Msts;
 
@@ -54,23 +51,42 @@ namespace CasaSoft.vrt.Modeling
     /// </summary>
     public class MstsShape : shape
     {
+        #region properties
+        /// <summary>
+        /// Minimum coordinate point
+        /// </summary>
+        public point MinPoint { get; protected set; }
+
+        /// <summary>
+        /// Maximum coordinate point
+        /// </summary>
+        public point MaxPoint { get; protected set; }
+        #endregion
+
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="block"></param>
         public MstsShape(SBR block) : base(block)
         {
-            AssociateMatrix();
+            MaxLod = GetMaxLod();
+            MatrixOfPoint = AssociateMatrix();
+            PrecalculatedMatrices = PrecalculateMatrices();
+            AbsolutePoints = ComputeAbsolutePoints();
+
+            MinPoint = GetMinPoint();
+            MaxPoint = GetMaxPoint();
         }
 
+        #region points computing
         /// <summary>
         /// Compute minimum of shape coords
         /// </summary>
         /// <returns></returns>
-        public point MinPoint()
+        protected point GetMinPoint()
         {
-            point ret = new point() { X = float.MaxValue, Y = float.MaxValue, Z = float.MaxValue };
-            foreach(point p in points)
+            point ret = new point(float.MaxValue, float.MaxValue, float.MaxValue);
+            foreach(point p in AbsolutePoints)
             {
                 ret.X = (p.X < ret.X ? p.X : ret.X);
                 ret.Y = (p.Y < ret.Y ? p.Y : ret.Y);
@@ -79,6 +95,24 @@ namespace CasaSoft.vrt.Modeling
             return ret;
         }
 
+        /// <summary>
+        /// Compute minimum of shape coords
+        /// </summary>
+        /// <returns></returns>
+        protected point GetMaxPoint()
+        {
+            point ret = new point(float.MinValue, float.MinValue, float.MinValue);
+            foreach (point p in AbsolutePoints)
+            {
+                ret.X = (p.X > ret.X ? p.X : ret.X);
+                ret.Y = (p.Y > ret.Y ? p.Y : ret.Y);
+                ret.Z = (p.Z > ret.Z ? p.Z : ret.Z);
+            }
+            return ret;
+        }
+        #endregion
+
+        #region precalculated values
         /// <summary>
         /// Association of point with matrix
         /// </summary>
@@ -96,28 +130,40 @@ namespace CasaSoft.vrt.Modeling
         }
 
         /// <summary>
+        /// Maximum level of detail
+        /// </summary>
+        protected distance_level MaxLod;
+
+        /// <summary>
+        /// Gets the maximum level of detail
+        /// </summary>
+        /// <returns></returns>
+        protected distance_level GetMaxLod()
+        {
+            // Find the maximum LOD
+            distance_level lod = null;
+            float max = 9999;
+            foreach (var l in lod_controls[0].distance_levels)
+            {
+                if (l.distance_level_header.dlevel_selection < max)
+                    lod = l;
+            }
+            return lod;
+        }
+        /// <summary>
         /// List of points with associated matrix
         /// </summary>
         protected List<PointAndMatrix> MatrixOfPoint;
 
         /// <summary>
-        /// Populates <see cref="M:CasaSoft.vrt.Modeling.MstsShape.MatrixOfPoint"/>
+        /// Populates <see cref="F:CasaSoft.vrt.Modeling.MstsShape.MatrixOfPoint"/>
         /// </summary>
-        protected void AssociateMatrix()
+        protected List<PointAndMatrix> AssociateMatrix()
         {
-            MatrixOfPoint = new List<PointAndMatrix>();
-
-            // Find the minimum LOD
-            distance_level lod = null;
-            float max = 9999;
-            foreach(var l in lod_controls[0].distance_levels)
-            {
-                if (l.distance_level_header.dlevel_selection < max)
-                    lod = l;
-            }
+            List<PointAndMatrix> mop = new List<PointAndMatrix>();
 
             // scan all subobjects
-            foreach(var s in lod.sub_objects)
+            foreach(var s in MaxLod.sub_objects)
             {
                 for(int j = 0; j < s.vertex_sets.Count; j++)
                 {
@@ -125,23 +171,58 @@ namespace CasaSoft.vrt.Modeling
                     for(int vj = 0; vj < vs.VtxCount; vj++)
                     {
                         vertex v = s.vertices[vs.StartVtxIdx + vj];
-                        MatrixOfPoint.Add(new PointAndMatrix() { iPoint = v.ipoint, iMatrix = vtx_states[vs.VtxStateIdx].imatrix });
+                        mop.Add(new PointAndMatrix() { iPoint = v.ipoint, iMatrix = vtx_states[vs.VtxStateIdx].imatrix });
                     }
                 }
             }
+            return mop;
         }
 
         /// <summary>
         /// Precalculated matrices based on hierarchy
         /// </summary>
-        protected matrix[] PrecalculatedMatrices;
+        protected MatrixEx[] PrecalculatedMatrices;
 
         /// <summary>
-        /// Populates <see cref="M:CasaSoft.vrt.Modeling.MstsShape.PrecalculatedMatrices"/>
+        /// Populates <see cref="F:CasaSoft.vrt.Modeling.MstsShape.PrecalculatedMatrices"/>
         /// </summary>
-        protected void PrecalculateMatrices()
+        protected MatrixEx[] PrecalculateMatrices()
         {
-            // todo
+            int n = MaxLod.distance_level_header.hierarchy.Length;
+            MatrixEx[] pm = new MatrixEx[n];
+            for (int i = 0; i < n; i++)
+            {
+                int hl = MaxLod.distance_level_header.hierarchy[i];
+                if(hl == -1)
+                {
+                    pm[i] = new MatrixEx(matrices[i]);
+                }
+                else
+                {
+                    pm[i] = new MatrixEx(pm[hl].Mul(matrices[i]));
+                }
+            }
+            return pm;
         }
+
+        /// <summary>
+        /// List of points with absolute coords
+        /// </summary>
+        protected List<point> AbsolutePoints;
+
+        /// <summary>
+        /// Populate <see cref="F:CasaSoft.vrt.Modeling.MstsShape.AbsolutePoints"/>
+        /// </summary>
+        /// <returns></returns>
+        protected List<point> ComputeAbsolutePoints()
+        {
+            List<point> ap = new List<point>();
+            foreach(var pm in MatrixOfPoint)
+            {
+                ap.Add(PrecalculatedMatrices[pm.iMatrix].Mul(points[pm.iPoint]));
+            }
+            return ap;
+        }
+        #endregion
     }
 }
